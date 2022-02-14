2520795bb6569051649e40e8e98f38e5ad103186
Nurul Mutmainnah <95833706+yuyuhere@users.noreply.github.com>
2022-02-15 03:08:39 +0800

    Add files via upload

--- java/db/ConnectionManager.java
+++ java/db/ConnectionManager.java
@@ -1,31 +1,37 @@
-package db;
+package view.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ *
+ * @author User
+ */
 public class ConnectionManager {
-	static Connection con;
-	private static final String DB_DRIVER= "oracle.jdbc.driver.OracleDriver";
-	private static final String DB_CONNECTION = "jdbc:oracle://localhost/takaCast";
-	private static final String DB_USER= "takaCast";
-	private static final String DB_PASSWORD="system";
-	
-	public static Connection getConnection() throws SQLException {
+    static Connection con;
+    private static final String DB_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private static final String DB_CONNECTION = "jdbc:oracle:thin:@localhost:1521:XE";
+    private static final String DB_USER = "taka";
+    private static final String DB_PASSWORD = "SYSTEM";
+                
+    
+    public static Connection getConnection() throws SQLException {
+        try {
+				
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+        con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE ", "taka","SYSTEM");
+				
 		try {
-			//load driver
-			Class.forName(DB_DRIVER);
-			con= DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","takaCast","system");
-			try {
-				//create connection
-				con = DriverManager.getConnection(DB_CONNECTION,DB_USER,DB_PASSWORD);
-				System.out.println("Connected");
-			}catch(SQLException e) {
-				e.printStackTrace();
-			}
-		}catch(ClassNotFoundException e) {
+			con = DriverManager.getConnection(DB_CONNECTION,DB_USER,DB_PASSWORD);
+            System.out.println("Connected");
+		}catch(SQLException e){
 			e.printStackTrace();
-		}
-		return con;
-	}	
+				}
+	}catch(ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+	return con;
+        
+    }
 }
