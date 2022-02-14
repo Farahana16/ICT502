8396444bbfa2afe8e651c8239910abb609118975
myzatulakmar126 <94593571+myzatulakmar126@users.noreply.github.com>
2022-02-15 03:23:10 +0800

    Update Feedback.java

--- java/model/Feedback.java
+++ java/model/Feedback.java
@@ -2,16 +2,16 @@ package model;
 
 public class Feedback {
 	int feedbackId;
-	int feedbackRating;
+    String  feedbackRating;
 	String feedbackComment;
 	int orderId;
 	
 	public int getFeedbackId() {return feedbackId;}
 	public String getFeedbackComment() {return feedbackComment;}
-	public int getFeedbackRating() {return feedbackRating;}
+	public String getFeedbackRating() {return feedbackRating;}
 	public int getOrderId() {return orderId;}
 	
-	public void setFeedbackRating(int feedbackRating) {this.feedbackRating = feedbackRating;}
+	public void setFeedbackRating(String feedbackRating) {this.feedbackRating = feedbackRating;}
 	public void setOrderId(int orderId) {this.orderId = orderId;}
 	public void setFeedbackId(int feedbackId) {this.feedbackId = feedbackId;}
 	public void setFeedbackComment(String feedbackComment) {this.feedbackComment = feedbackComment;}
